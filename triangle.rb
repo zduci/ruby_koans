b8fc4a47d8431b96c7706108134817f1ed32e8b6
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-16 10:38:08 +0000

    About triangle 2

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,27 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if not_a_triangle(a, b, c)
   return :equilateral if a == b && a == c
   return :isosceles if a == b || a == c || b == c
   :scalene
+end
+
+def not_a_triangle(a, b, c)
+  zero_or_negative_sides(a, b, c) || invalid_sides(a, b, c)
+end
+
+def zero_or_negative_sides(a, b, c)
+  zero_or_negative(a) || zero_or_negative(b) || zero_or_negative(c)
+end
+
+def zero_or_negative(x)
+  x <= 0
+end
+
+def invalid_sides(a, b, c)
+  sides = [a, b, c].sort
+  sides[0] + sides[1] <= sides[2]
 end
 
 # Error class used in part 2.  No need to change this code.
